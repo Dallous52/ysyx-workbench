cd309146678bf7f0a59f778c85c7e59422ac014f
tracer-ysyx <tracer@ysyx.org>
2025-03-15 10:46:37 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:46:37 up  1:24,  1 user,  load average: 0.00, 0.03, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include "Vtop.h"
+#include "verilated.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+int main(int argc, char** argv) 
+{
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+
+    Vtop* top = new Vtop{contextp};
+
+    // 初始化随机数发生器
+    srand((unsigned) time(&t));
+
+    while (!contextp->gotFinish())
+    {
+      int a = rand() & 1;
+      int b = rand() & 1;
+      top->a = a;
+      top->b = b;
+      top->eval();
+      printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+      assert(top->f == (a ^ b));
+    }
+
+    delete top;
+    delete contextp;
+
+    return 0;
 }
