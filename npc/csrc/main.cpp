7f2131886630649c7a316d49de8cc1da02366f84
tracer-ysyx <tracer@ysyx.org>
2025-03-17 14:45:30 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:30 up  1:05,  1 user,  load average: 0.20, 0.45, 0.29

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,37 +1,73 @@
+// #include "Vtop.h"
+// #include "verilated.h"
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <assert.h>
+// #include <time.h>
+// #include <unistd.h>
+
+// int main(int argc, char** argv) 
+// {
+//     VerilatedContext* contextp = new VerilatedContext;
+//     contextp->commandArgs(argc, argv);
+
+//     Vtop* top = new Vtop{contextp};
+
+//     // 初始化随机数发生器
+//     time_t t;
+//     srand((unsigned) time(&t));
+
+//     while (!contextp->gotFinish())
+//     {
+//       int a = rand() & 1;
+//       int b = rand() & 1;
+//       top->a = a;
+//       top->b = b;
+//       top->eval();
+//       printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+//       assert(top->f == (a ^ b));
+//       sleep(1);
+//     }
+
+//     delete top;
+//     delete contextp;
+
+//     return 0;
+// }
+
+#include <nvboard.h>
+
 #include "Vtop.h"
-#include "verilated.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <time.h>
-#include<unistd.h>
-
-int main(int argc, char** argv) 
+static void single_cycle(Vtop* dut) 
 {
-    VerilatedContext* contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
 
-    Vtop* top = new Vtop{contextp};
+static void reset(Vtop* dut, int n) 
+{
+  dut.rst = 1;
+  while (n-- > 0) single_cycle(dut);
+  dut.rst = 0;
+}
 
-    // 初始化随机数发生器
-    time_t t;
-    srand((unsigned) time(&t));
+int main()
+{
+  Vtop* top = new Vtop();
 
-    while (!contextp->gotFinish())
-    {
-      int a = rand() & 1;
-      int b = rand() & 1;
-      top->a = a;
-      top->b = b;
-      top->eval();
-      printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-      assert(top->f == (a ^ b));
-      sleep(1);
-    }
+	nvboard_bind_pin(&top->a, 1, SW0);
+	nvboard_bind_pin(&top->b, 1, SW1);
+	nvboard_bind_pin(&top->f, 1, LD0);
 
-    delete top;
-    delete contextp;
+  nvboard_init();
 
-    return 0;
+  reset(top, 10);
+
+  while(1) 
+  {
+    nvboard_update();
+    single_cycle(top);
+  }
 }
