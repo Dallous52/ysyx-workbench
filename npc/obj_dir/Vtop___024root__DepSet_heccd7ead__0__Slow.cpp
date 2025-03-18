5ae5de642b11a956ffba09d88158c52783877d7e
tracer-ysyx <tracer@ysyx.org>
2025-03-18 16:14:28 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:28 up 43 min,  1 user,  load average: 0.26, 0.45, 0.27

--- npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
@@ -48,7 +48,7 @@ VL_ATTR_COLD void Vtop___024root___eval_settle(Vtop___024root* vlSelf) {
 #ifdef VL_DEBUG
                 Vtop___024root___dump_triggers__stl(vlSelf);
 #endif
-                VL_FATAL_MT("/home/dallous/Documents/ysyx-workbench/npc/vsrc/top.v", 1, "", "Settle region did not converge.");
+                VL_FATAL_MT("vsrc/top.v", 1, "", "Settle region did not converge.");
             }
             vlSelf->__VstlIterCount = ((IData)(1U) 
                                        + vlSelf->__VstlIterCount);
@@ -128,7 +128,7 @@ VL_ATTR_COLD void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf) {
     Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
     VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___ctor_var_reset\n"); );
     // Body
-    vlSelf->a = 0;
-    vlSelf->b = 0;
-    vlSelf->f = 0;
+    vlSelf->a = VL_RAND_RESET_I(1);
+    vlSelf->b = VL_RAND_RESET_I(1);
+    vlSelf->f = VL_RAND_RESET_I(1);
 }
