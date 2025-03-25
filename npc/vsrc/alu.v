503a1c1976712b663a353b83ac851a491d558370
tracer-ysyx <tracer@ysyx.org>
2025-03-25 15:46:34 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:34 up 24 min,  1 user,  load average: 0.01, 0.12, 0.19

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -8,27 +8,30 @@ module alu(
     output reg carry
 );
     reg [3:0] cura, curb;
-    reg [3:0] tmp;
+    reg [3:0] tmp, ans;
 
     always @(*) begin
+        // 补码计算
         cura = arga[3] == 0 ? arga : ({arga[3], ~arga[2:0]} + 4'b0001);
         curb = argb[3] == 0 ? argb : ({argb[3], ~argb[2:0]} + 4'b0001);
         carry = 0;
         ovfl = 0;
         tmp = 0;
+        ans = 0;
 
         case (opt)
             3'b000: begin
-                {carry, res} = cura + curb;
-                ovfl = (cura[3] == curb[3]) && (res[3] != cura[3]);
-                res = res[3] == 0 ? res : {res[3], ~(res[2:0] - 3'b001)};
+                {carry, ans} = cura + curb;
+                ovfl = (cura[3] == curb[3]) && (ans[3] != cura[3]);
+                res = ans[3] == 0 ? ans : {ans[3], ~(ans[2:0] - 3'b001)};
             end
     
             3'b001: begin
+                // b -> -b
                 tmp = ~curb + 4'b0001;
-                {carry, res} = cura + tmp;
-                ovfl = (cura[3] == tmp[3]) && (res[3] != cura[3]);
-                res = res[3] == 0 ? res : {res[3], ~(res[2:0] - 3'b001)};
+                {carry, ans} = cura + tmp;
+                ovfl = (cura[3] == tmp[3]) && (ans[3] != cura[3]);
+                res = ans[3] == 0 ? ans : {ans[3], ~(ans[2:0] - 3'b001)};
             end
 
             3'b010: begin
