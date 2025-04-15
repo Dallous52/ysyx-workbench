72419f9451e5a98dd2042a530bd4c8c482770130
tracer-ysyx <tracer@ysyx.org>
2025-04-15 22:17:31 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:31 up 11:24,  1 user,  load average: 0.32, 0.13, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -29,10 +29,10 @@ int main(int argc, char** argv)
 
     while (true)
     { 
-        top->inst = paddr_read(top->pc, 4);
+        // top->inst = paddr_read(top->pc, 4);
         top->clk = 0; top->eval();
         top->clk = 1; top->eval();
-        // std::printf("%x\n", top->pc);
+        std::printf("%x\n", top->pc);
 #ifdef VCD_F
         vtrace->dump(sim_time++);
 #endif // VCD_F
