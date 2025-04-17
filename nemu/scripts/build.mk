3c486eb39429c1df3c2ae18e37cef7cf926516d1
tracer-ysyx <tracer@ysyx.org>
2025-04-17 18:37:36 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:37:36 up  1:36,  1 user,  load average: 0.33, 0.18, 0.19

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -31,7 +31,6 @@ OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
 # Compilation patterns
 $(OBJ_DIR)/%.o: %.c
-	@echo $(AM_CFLAGS) 
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
