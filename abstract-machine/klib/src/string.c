7475185d82bcbfea30ab73964b85bd6dfa4c4dd8
tracer-ysyx <tracer@ysyx.org>
2025-04-18 08:25:08 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:25:08 up  1:00,  1 user,  load average: 0.06, 0.07, 0.09

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,47 +1,48 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdint.h>
+#include <string.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  return strlen(s);
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  return strcpy(dst, src);        
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  return strncpy(dst, src, n);
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  return strcat(dst, src);        
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  return strcmp(s1, s2);
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  return strncmp(s1, s2, n);  
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  return memset(s, c, n);   
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  return memmove(dst, src, n);
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  return memcpy(out, in, n);
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  return memcmp(s1, s2, n);
 }
 
 #endif
