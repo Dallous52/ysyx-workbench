1cb3113a69009d2656e40de3780ee56ae10b7c79
tracer-ysyx <tracer@ysyx.org>
2025-04-19 09:14:35 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:14:35 up 28 min,  1 user,  load average: 0.00, 0.02, 0.04

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,17 +9,93 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+
+int sprintf(char *out, const char *fmt, ...) 
+{
+  // 获取可变参数
+  va_list args;
+  va_start(args, fmt);
+
+  char *p = out; // 当前写入位置
+
+  while (*fmt) 
+  {
+      if (*fmt == '%') 
+      {
+          fmt++;
+          switch (*fmt) 
+          {
+              case 'd': {
+                int num = va_arg(args, int);
+                char buf[20];
+                char *b = buf + sizeof(buf) - 1;
+                *b = '\0';
+
+                int neg = 0;
+                if (num < 0) { neg = 1; num = -num;}
+
+                do 
+                {
+                    *--b = '0' + (num % 10);
+                    num /= 10;
+                } while (num > 0);
+                
+                if (neg) *--b = '-';
+
+                while (*b) *p++ = *b++;
+
+                break;
+              }
+
+              case 's': {
+                  char *s = va_arg(args, char*);
+                  while (*s) *p++ = *s++;
+                  break;
+              }
+
+              case 'c': {
+                  // char参数提升为int，Default Argument Promotions(默认参数提升)
+                  char c = (char)va_arg(args, int); 
+                  *p++ = c;
+                  break;
+              }
+
+              case '%': {
+                  // 处理 %%
+                  *p++ = '%';
+                  break;
+              }
+
+              default: {
+                  // 遇到未知的格式，原样输出
+                  *p++ = '%';
+                  *p++ = *fmt;
+                  break;
+              }
+          }
+      } else {
+          *p++ = *fmt;
+      }
+      fmt++;
+  }
+
+  *p = '\0'; // 末尾加上 '\0'
+  va_end(args);
+  
+  // 返回总长
+  return p - out; 
 }
+
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
+
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
