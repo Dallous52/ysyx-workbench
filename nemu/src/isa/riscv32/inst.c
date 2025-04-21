de10636fcda77aec657c212c6a25657a66405d03
tracer-ysyx <tracer@ysyx.org>
2025-04-21 14:21:40 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:40 up  1:01,  1 user,  load average: 0.11, 0.05, 0.09

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -137,7 +137,8 @@ static int decode_exec(Decode *s) {
   return 0;
 }
 
-int isa_exec_once(Decode *s) {
+int isa_exec_once(Decode *s) 
+{
   s->isa.inst = inst_fetch(&s->snpc, 4);
   return decode_exec(s);
 }
