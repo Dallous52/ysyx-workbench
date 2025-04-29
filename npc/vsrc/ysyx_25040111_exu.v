975fc086ffa3ec9529bec43403f18df5c72355d3
tracer-ysyx <tracer@ysyx.org>
2025-04-29 15:52:02 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:02 up  1:46,  1 user,  load average: 0.27, 0.45, 0.25

--- npc/vsrc/ysyx_25040111_exu.v
+++ npc/vsrc/ysyx_25040111_exu.v
@@ -7,7 +7,8 @@ module ysyx_25040111_exu(
     output [31:0] rd_d
 );
 
-    ysyx_25040111_MuxKeyWithDefault #(3, 5, 32) rs1_add_imm (rd_d, opt[7:3], 32'b0, {
+    ysyx_25040111_MuxKeyWithDefault #(4, 5, 32) rs1_add_imm (rd_d, opt[7:3], 32'b0, {
+        5'b00000, imm,
         5'b00100, pc + imm,
         5'b00101, rs1_d + rs2_d,
         5'b00110, rs1_d + imm
--- npc/vsrc/ysyx_25040111_idu.v
+++ npc/vsrc/ysyx_25040111_idu.v
@@ -1,4 +1,4 @@
-`define OPCODE_NUM 2 
+`define OPCODE_NUM 3
 
 module ysyx_25040111_idu(
     input [31:0] inst,
@@ -15,7 +15,7 @@ module ysyx_25040111_idu(
     wire [4:0] rs1_opimm, rd_opimm;
     wire [31:0] imm_opimm;
     wire [7:0] opt_opimm;
-    
+
     ysyx_25040111_opimm u_ysyx_25040111_opimm(
         .inst 	(inst[31:7]),
         .rs1  	(rs1_opimm ),
@@ -39,6 +39,20 @@ module ysyx_25040111_idu(
     );
     
     // ------------------------------------------------------- 
+    //                         LUI          
+    // -------------------------------------------------------
+    wire [4:0] rd_lui;
+    wire [31:0] imm_lui;
+    wire [7:0] opt_lui;
+
+    ysyx_25040111_lui u_ysyx_25040111_lui(
+        .inst 	(inst[31:7]  ),
+        .rd   	(rd_lui    ),
+        .imm  	(imm_lui   ),
+        .opt  	(opt_lui   )
+    );
+
+    // ------------------------------------------------------- 
     //                         System                       
     // -------------------------------------------------------
     ysyx_25040111_system u_ysyx_25040111_system(
@@ -51,27 +65,32 @@ module ysyx_25040111_idu(
     // -------------------------------------------------------
     ysyx_25040111_MuxKeyWithDefault #(`OPCODE_NUM, 7, 5) rs1_c (rs1, inst[6:0], 5'b0, {
         7'b0010011, rs1_opimm,
-        7'b0010111, 5'b0
+        7'b0010111, 5'b0,
+        7'b0110111, 5'b0
     });
 
     ysyx_25040111_MuxKeyWithDefault #(`OPCODE_NUM, 7, 5) rs2_c (rs2, inst[6:0], 5'b0, {
         7'b0010011, 5'b0,
-        7'b0010111, 5'b0
+        7'b0010111, 5'b0,
+        7'b0110111, 5'b0
     });
 
     ysyx_25040111_MuxKeyWithDefault #(`OPCODE_NUM, 7, 5) rd_c (rd, inst[6:0], 5'b0, {
         7'b0010011, rd_opimm,
-        7'b0010111, rd_auipc
+        7'b0010111, rd_auipc,
+        7'b0110111, rd_lui
     });
 
     ysyx_25040111_MuxKeyWithDefault #(`OPCODE_NUM, 7, 32) imm_c (imm, inst[6:0], 32'b0, {
         7'b0010011, imm_opimm,
-        7'b0010111, imm_auipc
+        7'b0010111, imm_auipc,
+        7'b0110111, imm_lui
     });
 
     ysyx_25040111_MuxKeyWithDefault #(`OPCODE_NUM, 7, 8) opt_c (opt, inst[6:0], 8'b0, {
         7'b0010011, opt_opimm,
-        7'b0010111, opt_auipc
+        7'b0010111, opt_auipc,
+        7'b0110111, opt_lui
     });
 
 endmodule
