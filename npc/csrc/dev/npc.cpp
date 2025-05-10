6800ef6439380206ca0cf6c069e1999c319d0842
tracer-ysyx <tracer@ysyx.org>
2025-05-10 11:10:14 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousUbuntu 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:10:14 up  2:30,  1 user,  load average: 0.01, 0.04, 0.09

--- npc/csrc/dev/npc.cpp
+++ npc/csrc/dev/npc.cpp
@@ -233,3 +233,38 @@ extern "C" void ebreak(int code)
 
     finalize(code);
 }
+
+
+extern "C" int pmem_read(int raddr)
+{
+    printf(ANSI_FMT("[read ] address = 0x%08x; pc = 0x%08x;\n", ANSI_FG_CYAN),
+        (paddr_t)raddr & ~0x3u, top.pc);
+    // 总是读取地址为`raddr & ~0x3u`的4字节返回
+    return (int)paddr_read((paddr_t)raddr & ~0x3u, 4);
+}
+
+
+extern "C" void pmem_write(int waddr, int wdata, char wmask) 
+{
+    // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
+    // `wmask`中每比特表示`wdata`中1个字节的掩码,
+    // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+ 
+    paddr_t address = waddr & ~0x3u;
+    printf(ANSI_FMT("[write ] address = 0x%08x; pc = 0x%08x; mask: 0x%02x;\n", ANSI_FG_CYAN),
+    (paddr_t)waddr, top.pc, wmask);
+    switch (wmask) 
+    {
+    case 0x1:
+        paddr_write(address, 1, (word_t)wdata);
+        break;
+    case 0x3:
+        paddr_write(address, 2, (word_t)wdata);
+        break;
+    case 0xF:
+        paddr_write(address, 4, (word_t)wdata);
+        break;
+    default:
+        break;
+    }
+}
