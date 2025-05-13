296cba59f391fcd383272235ead3e1a52f10266b
tracer-ysyx <tracer@ysyx.org>
2025-05-13 16:55:38 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:55:38 up  1:53,  1 user,  load average: 0.11, 0.07, 0.01

---
+++ npc/vsrc/ysyx_25040111_branch.v
@@ -0,0 +1,27 @@
+`include "ysyx_25040111_inc.vh"
+
+module ysyx_25040111_branch(
+    input [31:7] inst,
+    output [4:0] rs1,
+    output [4:0] rs2,
+    output [31:0] imm,
+    output [`OPT_HIGH:0] opt
+);
+
+    wire [12:0] imm_m;
+    wire [2:0] fun3;
+
+    assign imm_m[12] = inst[31];
+    assign imm_m[10:5] = inst[30:25];
+    assign imm_m[4:1] = inst[11:8];
+    assign imm_m[11] = inst[7];
+    assign imm_m[0] = 1'b0;
+
+    assign {rs2, rs1, fun3} = inst[24:12];
+    assign imm = {{19{imm_m[12]}}, imm_m};
+
+    ysyx_25040111_MuxKeyWithDefault #(1, 3, `OPT_LEN) opt_c (opt, fun3, `OPT_LEN'b0, {
+        3'b000, `OPTG(`XFS, `RF_RS, `EQUAL, `BRANCH, `EMPTY, `EXX)  // beq
+    });
+
+endmodule
