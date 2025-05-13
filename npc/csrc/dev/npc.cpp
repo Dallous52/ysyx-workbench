7f9db7abfaae39449188db953fd74aab1388625e
tracer-ysyx <tracer@ysyx.org>
2025-05-13 21:09:15 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:09:15 up  6:16,  1 user,  load average: 0.08, 0.07, 0.06

--- npc/csrc/dev/npc.cpp
+++ npc/csrc/dev/npc.cpp
@@ -99,7 +99,7 @@ int cpu_exec(uint64_t steps)
 
         ftrace(currpc, top.pc);
         check_wp();
-        if (!difftest_step(currpc)) npc_stat = NPC_STOP;
+        // if (!difftest_step(currpc)) npc_stat = NPC_STOP;
 
         switch (npc_stat)
         {
@@ -240,7 +240,7 @@ extern "C" void ebreak(int code)
 
 extern "C" int pmem_read(int raddr)
 {
-    paddr_t address = (word_t)raddr; // raddr & ~0x3u;
+    paddr_t address = raddr & ~0x3u;
     word_t rdata = paddr_read(address, 4);
 
     // mtrace memory read
