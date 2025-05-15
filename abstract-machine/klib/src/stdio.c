fc4bf76d1ae688f933b61323436e825e8a82d0fa
tracer-ysyx <tracer@ysyx.org>
2025-05-15 16:25:17 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:25:17 up  4:22,  1 user,  load average: 0.09, 0.12, 0.09

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -19,84 +19,117 @@ int printf(const char *fmt, ...) {
 }
 
 
-int vsprintf(char *out, const char *fmt, va_list ap) 
-{
-  char *p = out; // 当前写入位置
 
-  while (*fmt) 
-  {
-      if (*fmt == '%') 
-      {
-          fmt++;
-          switch (*fmt) 
-          {
-              case 'd': {
-                int num = va_arg(ap, int);
-                char buf[20];
-                char *b = buf + sizeof(buf) - 1;
-                *b = '\0';
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  char *p = out;
 
-                int neg = 0;
-                if (num < 0) 
-                { 
-                  neg = 1; 
-                  if (num == INT_MIN)
-                  {
-                    strcpy(p, "-2147483648");
-                    p += 11;
-                    break;
-                  }
-                  num = -num; 
-                }
+  while (*fmt) {
+    if (*fmt != '%') {
+      *p++ = *fmt++;
+      continue;
+    }
 
-                do 
-                {
-                    *--b = '0' + (num % 10);
-                    num /= 10;
-                } while (num > 0);
-                
-                if (neg) *--b = '-';
+    fmt++;  // skip '%'
 
-                while (*b) *p++ = *b++;
-
-                break;
-              }
-
-              case 's': {
-                  char *s = va_arg(ap, char*);
-                  while (*s) *p++ = *s++;
-                  break;
-              }
-
-              case 'c': {
-                  // char参数提升为int，Default Argument Promotions(默认参数提升)
-                  char c = (char)va_arg(ap, int); 
-                  *p++ = c;
-                  break;
-              }
-
-              case '%': {
-                  // 处理 %%
-                  *p++ = '%';
-                  break;
-              }
-
-              default: {
-                  // 遇到未知的格式，原样输出
-                  *p++ = '%';
-                  *p++ = *fmt;
-                  break;
-              }
-          }
-      } else {
-          *p++ = *fmt;
-      }
+    // ----------- 解析补零和宽度 -----------
+    int zero_pad = 0, width = 0;
+    if (*fmt == '0') {
+      zero_pad = 1;
       fmt++;
+    }
+    while (*fmt >= '0' && *fmt <= '9') {
+      width = width * 10 + (*fmt++ - '0');
+    }
+
+    // ----------- 格式处理 -----------
+    switch (*fmt) {
+      case 'd': {
+        int num = va_arg(ap, int);
+        char buf[20], *b = buf + sizeof(buf) - 1;
+        *b = '\0';
+        int neg = 0;
+
+        if (num < 0) {
+          neg = 1;
+          if (num == INT_MIN) {
+            // 特殊处理最小负数
+            strcpy(buf, "-2147483648");
+            b = buf;
+            break;
+          }
+          num = -num;
+        }
+
+        do {
+          *--b = '0' + (num % 10);
+          num /= 10;
+        } while (num);
+
+        if (neg) *--b = '-';
+
+        int len = strlen(b);
+        while (len < width) {
+          *p++ = (zero_pad && !neg) ? '0' : ' ';
+          width--;
+        }
+        while (*b) *p++ = *b++;
+        break;
+      }
+
+      case 'x': {
+        unsigned int num = va_arg(ap, unsigned int);
+        char buf[20], *b = buf + sizeof(buf) - 1;
+        *b = '\0';
+        const char *hex = "0123456789abcdef";
+
+        do {
+          *--b = hex[num % 16];
+          num /= 16;
+        } while (num);
+
+        int len = strlen(b);
+        while (len < width) {
+          *p++ = zero_pad ? '0' : ' ';
+          width--;
+        }
+        while (*b) *p++ = *b++;
+        break;
+      }
+
+      case 's': {
+        char *s = va_arg(ap, char *);
+        int len = strlen(s);
+        while (len < width) {
+          *p++ = zero_pad ? '0' : ' ';
+          width--;
+        }
+        while (*s) *p++ = *s++;
+        break;
+      }
+
+      case 'c': {
+        char c = (char)va_arg(ap, int);
+        *p++ = c;
+        break;
+      }
+
+      case '%': {
+        *p++ = '%';
+        break;
+      }
+
+      default: {
+        *p++ = '%';
+        *p++ = *fmt;
+        break;
+      }
+    }
+
+    fmt++;
   }
 
-  *p = '\0'; // 末尾加上 '\0'
-
-  return p - out; 
+  *p = '\0';
+  return p - out;
 }
 
 
