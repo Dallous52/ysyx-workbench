5f4e71e4a0d94cc82a24ab746be2a6591e507f3f
tracer-ysyx <tracer@ysyx.org>
2025-05-20 13:12:34 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:12:34 up  4:06,  1 user,  load average: 0.03, 0.07, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,6 +1,7 @@
 #include "klib.h"
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
@@ -9,14 +10,13 @@ void __am_gpu_init()
 {
   int w = inw(VGACTL_ADDR + 2);
   int h = inw(VGACTL_ADDR);
-  int i;
-  uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i++) fb[i] = i;
   printf("w:%d  h:%d\n", w, h);
 }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
+{
+  *cfg = (AM_GPU_CONFIG_T) 
+  {
     .present = true, .has_accel = false,
     .width = 0, .height = 0,
     .vmemsz = 0
@@ -26,8 +26,29 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   cfg->vmemsz = cfg->width * cfg->height;
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) 
+{
+  if (ctl->sync) 
+  {
+    int i = 0, j = 0, k = 0;
+
+    int w = inw(VGACTL_ADDR + 2);
+
+    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+    uint32_t* px = (uint32_t*)ctl->pixels;
+
+    int mx = ctl->x + ctl->w;
+    int my = ctl->y + ctl->h;
+
+    for (i = ctl->y; i < my; i++)
+    {
+      for (j = ctl->x; j < mx; j++)
+      {
+        fb[i * w + j] = px[k++];
+      }
+    }
+    
     outl(SYNC_ADDR, 1);
   }
 }
