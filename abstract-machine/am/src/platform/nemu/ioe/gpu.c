09d2b26941e341b3b32201edc924cda362576641
tracer-ysyx <tracer@ysyx.org>
2025-05-20 10:32:55 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:32:55 up  1:30,  1 user,  load average: 0.20, 0.16, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -13,6 +13,7 @@ void __am_gpu_init()
   uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
   for (i = 0; i < w* h; i++) fb[i] = i;
   outl(SYNC_ADDR, 1);
+  printf("w:%d  h:%d\n", w, h);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
