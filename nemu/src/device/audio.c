a6818345a994f123a967b403a8fb9c125d2d6c0e
tracer-ysyx <tracer@ysyx.org>
2025-05-21 15:43:05 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:43:05 up  2:54,  1 user,  load average: 0.23, 0.23, 0.15

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -37,9 +37,9 @@ void audio_callback(void *userdata, Uint8 *stream, int len)
   static uint32_t audio_pos = 0;
 
   printf("count %d\n",audio_base[reg_count]);
+  printf("pos %d\n",audio_pos);
   if (audio_pos >= audio_len) {
     // 没有数据了，填充静音
-    printf("pos %d\n",audio_pos);
     SDL_memset(stream, 0, len);
     return;
   }
