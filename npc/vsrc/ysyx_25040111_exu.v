9e4cbd5d37e4904690f9d509cb25ae729a442e5a
tracer-ysyx <tracer@ysyx.org>
2025-05-23 17:09:28 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:09:28 up  9:34,  1 user,  load average: 0.02, 0.08, 0.13

--- npc/vsrc/ysyx_25040111_exu.v
+++ npc/vsrc/ysyx_25040111_exu.v
@@ -116,6 +116,7 @@ module ysyx_25040111_exu(
     wire [31:0] eret;
     assign eret = opt[15] ? rs1_d : 32'd9;
     always @(*) begin
+        $display("%b\n", opt);
         if (opt == `EBREAK_INST)
             ebreak(eret);
     end
