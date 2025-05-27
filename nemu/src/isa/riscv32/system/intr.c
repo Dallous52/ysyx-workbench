3fde112b3cc7493c206ffe39c05b0b48bd54ee41
tracer-ysyx <tracer@ysyx.org>
2025-05-27 14:30:29 +0800

    > compile NEMU ysyx_22040000 胡昊 Linux DallousMain 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:30:29 up  5:55,  1 user,  load average: 0.41, 0.25, 0.10

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -24,8 +24,12 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc)
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  // printf("mtvec 0x%08x; mcause 0x%08x; mepc 0x%08x\n", 
-  //    CSR(MTVEC), NO, epc);
+
+#ifdef CONFIG_ETRACE
+   printf("mtvec 0x%08x; mcause 0x%08x; mepc 0x%08x\n", 
+      CSR(MTVEC), NO, epc);
+#endif
+
   CSR(MEPC) = epc;
   CSR(MCAUSE) = NO;
   
