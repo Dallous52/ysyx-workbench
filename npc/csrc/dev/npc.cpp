4e904605e9295f279f64a33d896710b05368b323
tracer-ysyx <tracer@ysyx.org>
2025-06-08 09:54:34 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:54:34 up  1:29,  1 user,  load average: 0.66, 0.34, 0.19

--- npc/csrc/dev/npc.cpp
+++ npc/csrc/dev/npc.cpp
@@ -90,15 +90,15 @@ int cpu_exec(uint64_t steps)
     {
         currpc = top.pc;
         instruct = paddr_read(top.pc, 4);
-        print_exe_info(currpc, instruct, logbuf, 128);
-        printf("%s\n", logbuf);
+        // print_exe_info(currpc, instruct, logbuf, 128);
+        // printf("%s\n", logbuf);
 
         top.clk = 0; top.eval();
         if (vtrace) vtrace->dump(sim_time++);
         top.clk = 1; top.eval();
         if (vtrace) vtrace->dump(sim_time++);
 
-        ftrace(currpc, top.pc);
+        // ftrace(currpc, top.pc);
         // check_wp();
         // if (!difftest_step(currpc)) npc_stat = NPC_STOP;
 
@@ -243,9 +243,6 @@ extern "C" int pmem_read(int raddr)
 {
     paddr_t address = raddr & ~0x3u;
     word_t rdata = 0;
-    
-    // printf(ANSI_FMT("[read mem] address: 0x%08x; data: 0x%08x; pc: 0x%08x;\n", ANSI_FG_CYAN),
-    //     (word_t)raddr, rdata, top.pc);
 
     if (likely(in_pmem(address)))
         rdata = paddr_read(address, 4);
@@ -253,12 +250,12 @@ extern "C" int pmem_read(int raddr)
         finalize(2);
 
     // mtrace memory read
-    word_t minst = paddr_read(top.pc, 4);
-    if (raddr != top.pc && 0b0000011 == BITS(minst, 6, 0))
-    {
-        printf(ANSI_FMT("[read mem] address: 0x%08x; data: 0x%08x; pc: 0x%08x;\n", ANSI_FG_CYAN),
-            (word_t)raddr, rdata, top.pc);
-    }
+    // word_t minst = paddr_read(top.pc, 4);
+    // if (raddr != top.pc && 0b0000011 == BITS(minst, 6, 0))
+    // {
+    //     printf(ANSI_FMT("[read mem] address: 0x%08x; data: 0x%08x; pc: 0x%08x;\n", ANSI_FG_CYAN),
+    //         (word_t)raddr, rdata, top.pc);
+    // }
 
     // 总是读取地址为`raddr & ~0x3u`的4字节返回
     return (int)rdata;
@@ -296,12 +293,12 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask)
     paddr_t address = waddr & ~0x3u;
 
     // mtrace memory write
-    word_t minst = paddr_read(top.pc, 4);
-    if (0b0100011 == BITS(minst, 6, 0))
-    {
-        printf(ANSI_FMT("[write mem] address: 0x%08x; data: 0x%08x; pc: 0x%08x; mask: 0x%02x;\n", ANSI_FG_CYAN),
-           (paddr_t)waddr, (word_t)wdata, top.pc, wmask);
-    }
+    // word_t minst = paddr_read(top.pc, 4);
+    // if (0b0100011 == BITS(minst, 6, 0))
+    // {
+    //     printf(ANSI_FMT("[write mem] address: 0x%08x; data: 0x%08x; pc: 0x%08x; mask: 0x%02x;\n", ANSI_FG_CYAN),
+    //        (paddr_t)waddr, (word_t)wdata, top.pc, wmask);
+    // }
 
     if (likely(in_pmem(address))) { pmem_write_core(address, wdata, wmask); return; }
     if (device_call((paddr_t)waddr, &wdata, true)) return;
