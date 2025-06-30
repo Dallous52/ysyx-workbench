34e3ddb625acd3db3ca520e6d86e2108c7aeb684
tracer-ysyx <tracer@ysyx.org>
2025-06-30 16:32:32 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:32:32 up  8:45,  1 user,  load average: 0.10, 0.07, 0.07

--- npc/vsrc/ysyx_25040111_lsu.v
+++ npc/vsrc/ysyx_25040111_lsu.v
@@ -28,15 +28,15 @@ module ysyx_25040111_lsu (
     }); 
 
     always @(*) begin
-        if (wen)
-            pmem_write(addr, wdata, wmask);
+        if (wen) begin
+            pmem_write(addr, wmem, wmask);          
+        end
     end
 
     reg [31:0] rmem;
     always @(*) begin
         if (ren) begin
             rmem = pmem_read(addr);
-            $display("addr:%h rmem: %h  rdata: %h", addr, rmem, rdata);
         end
         else 
             rmem = 32'b0;
