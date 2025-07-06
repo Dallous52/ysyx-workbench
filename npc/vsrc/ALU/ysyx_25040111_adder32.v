af7efc044f94b00f14fad62572f268daef055f4f
tracer-ysyx <tracer@ysyx.org>
2025-07-06 19:53:14 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:53:14 up 11:58,  1 user,  load average: 0.16, 0.11, 0.09

--- npc/vsrc/ALU/ysyx_25040111_adder32.v
+++ npc/vsrc/ALU/ysyx_25040111_adder32.v
@@ -10,26 +10,34 @@ module ysyx_25040111_adder32 (
     output cout
 );
 
-    wire [7:0] cin;
+    // wire [7:0] cin;
+    // wire [31:0] inbt;
+    // assign inbt = sub ? ~inb : inb;
+    
+    // generate
+    //     genvar i;
+    //     for (i = 0; i < 8; i++) begin
+    //         wire gcin = i == 0 ? sub : cin[i - 1];
+    //         ysyx_25040111_adder4 u_ysyx_25040111_adder4(
+    //             .ina  	(ina[(4 * i + 3):(4 * i)]),
+    //             .inb  	(inbt[(4 * i + 3):(4 * i)]),
+    //             .cin  	(gcin),
+    //             .cout 	(cin[i]),
+    //             .sout 	(sout[(4 * i + 3):(4 * i)])
+    //         );
+    //     end
+    // endgenerate
+
+    // // 无符号数进位
+    // assign cout = cin[7];
+    // // 有符号溢出：两个操作数符号相同，结果符号不同
+    // assign overflow = (~(ina[31] ^ inbt[31])) & (ina[31] ^ sout[31]);
+
     wire [31:0] inbt;
     assign inbt = sub ? ~inb : inb;
     
-    generate
-        genvar i;
-        for (i = 0; i < 8; i++) begin
-            wire gcin = i == 0 ? sub : cin[i - 1];
-            ysyx_25040111_adder4 u_ysyx_25040111_adder4(
-                .ina  	(ina[(4 * i + 3):(4 * i)]),
-                .inb  	(inbt[(4 * i + 3):(4 * i)]),
-                .cin  	(gcin),
-                .cout 	(cin[i]),
-                .sout 	(sout[(4 * i + 3):(4 * i)])
-            );
-        end
-    endgenerate
-
     // 无符号数进位
-    assign cout = cin[7];
+    assign {cout, sout} = ina + inbt;
     // 有符号溢出：两个操作数符号相同，结果符号不同
     assign overflow = (~(ina[31] ^ inbt[31])) & (ina[31] ^ sout[31]);
 
