87959023f5f0ab6205103037a50a00a15d7e1f54
tracer-ysyx <tracer@ysyx.org>
2025-07-06 19:39:04 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:39:04 up 11:43,  1 user,  load average: 0.15, 0.11, 0.03

--- npc/vsrc/ALU/ysyx_25040111_alu.v
+++ npc/vsrc/ALU/ysyx_25040111_alu.v
@@ -14,54 +14,29 @@ module ysyx_25040111_alu (
     output [31:0] res
 );
 
-    // wire [31:0] res_add;
-    // wire [31:0] vart, dxor;
-    // wire overflow, cout, lt;
+    wire [31:0] res_add;
+    wire [31:0] vart, dxor;
+    wire overflow, cout, lt;
 
-    // assign vart = snpc ? 32'd4 : var2;
+    assign vart = snpc ? 32'd4 : var2;
 
-    // ysyx_25040111_adder32 u_adder32(
-    //     .ina  	    (var1   ),
-    //     .inb  	    (vart   ),
-    //     .sub        (ext),
-    //     .sout 	    (res_add),
-    //     .overflow   (overflow),
-    //     .cout       (cout)
-    // );
-
-    // assign dxor = (var1 ^ var2);
-    // assign lt = sign ? res_add[31] ^ overflow : ~cout;
-    // wire signed [31:0] shiftrl;
-    // assign shiftrl = $signed({var1[31] & sign, 31'b0}) >>> var2[4:0];
-
-    // ysyx_25040111_MuxKey #(8, 3, 32) c_alu (res, opt, {
-    //     3'b000, var1,
-    //     3'b001, res_add, 
-    //     3'b010, ext ? (var1 | var2) : (var1 & var2),
-    //     3'b011, dxor,
-    //     3'b100, var1 << var2[4:0],
-    //     3'b101, (var1 >> var2[4:0]) | shiftrl,
-    //     3'b110, {31'b0, lt ^ negate},
-    //     3'b111, {31'b0, (~(|dxor)) ^ negate}
-    // });
-    
-    wire [31:0] b;
-    wire [31:0] add_result;
-    wire [31:0] dxor;
-    wire lt;
-
-    assign b = ext ? var2 : ~var2;
-    wire carry;
-    assign carry = ext ? 1'b0 : 1'b1;
-    assign add_result = var1 + b + {31'b0, carry};
+    ysyx_25040111_adder32 u_adder32(
+        .ina  	    (var1   ),
+        .inb  	    (vart   ),
+        .sub        (ext),
+        .sout 	    (res_add),
+        .overflow   (overflow),
+        .cout       (cout)
+    );
 
     assign dxor = (var1 ^ var2);
-    assign lt = var1 < var2;
+    assign lt = sign ? res_add[31] ^ overflow : ~cout;
     wire signed [31:0] shiftrl;
     assign shiftrl = $signed({var1[31] & sign, 31'b0}) >>> var2[4:0];
+
     ysyx_25040111_MuxKey #(8, 3, 32) c_alu (res, opt, {
         3'b000, var1,
-        3'b001, add_result, 
+        3'b001, res_add, 
         3'b010, ext ? (var1 | var2) : (var1 & var2),
         3'b011, dxor,
         3'b100, var1 << var2[4:0],
@@ -69,6 +44,31 @@ module ysyx_25040111_alu (
         3'b110, {31'b0, lt ^ negate},
         3'b111, {31'b0, (~(|dxor)) ^ negate}
     });
+    
+    // wire [31:0] b;
+    // wire [31:0] add_result;
+    // wire [31:0] dxor;
+    // wire lt;
+
+    // assign b = ext ? var2 : ~var2;
+    // wire carry;
+    // assign carry = ext ? 1'b0 : 1'b1;
+    // assign add_result = var1 + b + {31'b0, carry};
+
+    // assign dxor = (var1 ^ var2);
+    // assign lt = var1 < var2;
+    // wire signed [31:0] shiftrl;
+    // assign shiftrl = $signed({var1[31] & sign, 31'b0}) >>> var2[4:0];
+    // ysyx_25040111_MuxKey #(8, 3, 32) c_alu (res, opt, {
+    //     3'b000, var1,
+    //     3'b001, add_result, 
+    //     3'b010, ext ? (var1 | var2) : (var1 & var2),
+    //     3'b011, dxor,
+    //     3'b100, var1 << var2[4:0],
+    //     3'b101, (var1 >> var2[4:0]) | shiftrl,
+    //     3'b110, {31'b0, lt ^ negate},
+    //     3'b111, {31'b0, (~(|dxor)) ^ negate}
+    // });
 
 endmodule
 
