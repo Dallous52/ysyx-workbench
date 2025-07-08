b8bc460775301d5b640e20b6aa65ad2d326d8f11
tracer-ysyx <tracer@ysyx.org>
2025-07-08 11:17:20 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:17:20 up  2:45,  1 user,  load average: 0.29, 0.38, 0.42

--- npc/vsrc/ysyx_25040111_lsu.v
+++ npc/vsrc/ysyx_25040111_lsu.v
@@ -141,7 +141,7 @@ module ysyx_25040111_lsu (
     // memory read
     // assign rready = 1;
     always @(posedge clk) begin
-        $display("is_clint:%b  arvalid:%b  arready:%b  rvalid:%b  rready:%b", is_clint, arvalid, arready, rvalid, rready);
+        $display("is_clint:%b  arvalid:%b  arready:%b  rvalid:%b  rready:%b", is_clint, arvalid, arready, io_master_rvalid, rready);
         // 地址有效
         if (ren & ready)
             arvalid <= 1;
