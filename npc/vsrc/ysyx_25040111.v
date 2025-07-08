ffa544cfa76643df33cd8477d8170a02bb6fc76c
tracer-ysyx <tracer@ysyx.org>
2025-07-08 10:36:53 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:36:53 up  2:04,  1 user,  load average: 0.20, 0.21, 0.16

--- npc/vsrc/ysyx_25040111.v
+++ npc/vsrc/ysyx_25040111.v
@@ -221,11 +221,11 @@ module ysyx_25040111(
         .rd_d  	(rd_dt  )
     );
 
-    // always @(posedge clock) begin
-    //     $display("opt: %b", opt);
-    //     $display("ready:%b  ren:%b  wen:%b  addr:%h  if_ok:%b", lsu_ready, lsu_ren, lsu_wen, lsu_addr, lsu_ok);
-    //     $display("inst:%b  args:%b  next:%b", inst_ok, args_ok, next_ok);
-    // end
+    always @(posedge clock) begin
+        $display("opt: %b", opt);
+        $display("ready:%b  ren:%b  wen:%b  addr:%h  if_ok:%b", lsu_ready, lsu_ren, lsu_wen, lsu_addr, lsu_ok);
+        $display("inst:%b  args:%b  next:%b", inst_ok, args_ok, next_ok);
+    end
 
     ysyx_25040111_pcu u_ysyx_25040111_pcu(
         .clk          (clock        ),
