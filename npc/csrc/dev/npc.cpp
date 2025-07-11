558cba1e5759dcac83fa2fc415d1747a320079be
tracer-ysyx <tracer@ysyx.org>
2025-07-11 10:25:26 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:25:26 up  1:59,  1 user,  load average: 0.43, 0.22, 0.19

--- npc/csrc/dev/npc.cpp
+++ npc/csrc/dev/npc.cpp
@@ -14,8 +14,8 @@
 #define CPU_PC    (top.rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__pc)
 #define ADDR     (top.rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu_addr)
 
-// #define EN_TRACE
-// #define ITRACE
+#define EN_TRACE
+#define ITRACE
 // #define FTRACE
 // #define MTRACE
 // #define DIFFTEST
