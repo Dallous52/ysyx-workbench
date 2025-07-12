fbbcd7e7e7ed7df344183372c22e7e442170583c
tracer-ysyx <tracer@ysyx.org>
2025-07-12 19:18:57 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:18:57 up 10:53,  1 user,  load average: 0.45, 0.27, 0.16

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -15,6 +15,7 @@ int main(const char *args);
 
 #define DEV_SERIAL  (0x10000000)
 #define DEV_SRAM    (0x0f000000)
+#define DEV_SPI     (0x10001000)
 
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
@@ -51,17 +52,40 @@ void uart_init()
   *uart_lcr = 0x83;
 
   volatile uint8_t* uart_divisor = (volatile uint8_t*)DEV_SERIAL;
-  uart_divisor[1] = 0x01;
+  uart_divisor[1] = 0x00;
   uart_divisor[0] = 0x46;
 
   *uart_lcr = 0x03;
 }
 
 
-void _trm_init() 
+void spi_init()
+{
+  volatile uint8_t* spi_ctrl = (volatile uint8_t*)(DEV_SPI + 0x10);
+  spi_ctrl[3] = 0x00;
+  spi_ctrl[2] = 0x00;
+  spi_ctrl[1] = 0x28;
+  spi_ctrl[0] = 0x08;
+  
+  volatile uint8_t* spi_divider = (volatile uint8_t*)(DEV_SPI + 0x14);
+  spi_divider[3] = 0x46;
+  spi_divider[2] = 0;
+  spi_divider[1] = 0;
+  spi_divider[0] = 0;
+
+  volatile uint8_t* spi_ss = (volatile uint8_t*)(DEV_SPI + 0x18);
+  spi_ss[3] = 0;
+  spi_ss[2] = 0;
+  spi_ss[1] = 0;
+  spi_ss[0] = 0x07;
+}
+
+
+void _trm_init()
 {
   bootloader();
   uart_init();
+  spi_init();
   int ret = main(mainargs);
   halt(ret);
 }
