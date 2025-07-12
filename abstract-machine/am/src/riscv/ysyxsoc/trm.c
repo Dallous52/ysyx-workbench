754d4a8d0ed2a3729a3db15da0c2f9557e6511ac
tracer-ysyx <tracer@ysyx.org>
2025-07-12 20:21:09 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:21:09 up 11:56,  1 user,  load average: 0.77, 0.50, 0.29

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -65,7 +65,7 @@ void spi_init()
   spi_ctrl[3] = 0x00;
   spi_ctrl[2] = 0x00;
   spi_ctrl[1] = 0x28;
-  spi_ctrl[0] = 0x10;
+  spi_ctrl[0] = 0x08;
   
   volatile uint8_t* spi_divider = (volatile uint8_t*)(DEV_SPI + 0x14);
   spi_divider[3] = 0;
