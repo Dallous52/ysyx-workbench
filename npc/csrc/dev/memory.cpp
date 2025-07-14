bf06fa3aa39aefdf8ed06c361fe8cbb857a85474
tracer-ysyx <tracer@ysyx.org>
2025-07-14 15:13:25 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:13:25 up  7:00,  1 user,  load average: 0.25, 0.32, 0.34

--- npc/csrc/dev/memory.cpp
+++ npc/csrc/dev/memory.cpp
@@ -161,11 +161,11 @@ bool pmem_init(const char* fbin)
 extern "C" void flash_read(int32_t addr, int32_t *data) 
 {
   uint32_t address = (addr & ~0x3u) + MBASE;
-  printf(ANSI_FMT("load flash 0x%08x  ", ANSI_FG_GREEN), address);
+  // printf(ANSI_FMT("load flash 0x%08x  ", ANSI_FG_GREEN), address);
   if (likely(in_pmem(address)))
   {
     *data = paddr_read(address, 4);
-    printf(ANSI_FMT("data 0x%08x.\n", ANSI_FG_GREEN), *data);
+    // printf(ANSI_FMT("data 0x%08x.\n", ANSI_FG_GREEN), *data);
     return;
   }
 
