5ca4b144d50c9be7966750a9ebfd15b14cf1e10c
tracer-ysyx <tracer@ysyx.org>
2025-07-16 09:51:51 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:51:51 up 35 min,  1 user,  load average: 0.32, 0.14, 0.09

--- npc/csrc/dev/psram.cpp
+++ npc/csrc/dev/psram.cpp
@@ -9,8 +9,8 @@ static uint8_t psram[4*1024*1024]  __attribute((aligned(4096))) = {};
 extern "C" void psram_read(int32_t addr, int32_t *data)
 {
     memcpy(data, psram + addr, 4);
-    printf(ANSI_FMT("[read psram] address: 0x%08x; data: 0x%08x;\n", ANSI_FG_CYAN),
-			(paddr_t)addr, *data);
+    // printf(ANSI_FMT("[read psram] address: 0x%08x; data: 0x%08x;\n", ANSI_FG_CYAN),
+	// 		(paddr_t)addr, *data);
 }
 
 
@@ -22,9 +22,9 @@ extern "C" void psram_write(int32_t addr, int32_t data, int32_t len)
         // printf("%08x  %08x\n", addr + len, data >> right);
         psram[addr + len - 1] = data >> right;
         right -= 8;
-        len--;         
+        len--;
     }
 
-    printf(ANSI_FMT("[write psram] address: 0x%08x; data: 0x%08x;\n", ANSI_FG_CYAN),
-        (paddr_t)addr, data);
+    // printf(ANSI_FMT("[write psram] address: 0x%08x; data: 0x%08x;\n", ANSI_FG_CYAN),
+    //     (paddr_t)addr, data);
 }
