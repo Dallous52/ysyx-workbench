7771fd412c4139bb91b22784ddd5827e13ec9cbd
tracer-ysyx <tracer@ysyx.org>
2025-07-17 15:31:58 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:31:58 up  6:22,  1 user,  load average: 0.06, 0.10, 0.09

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -13,14 +13,39 @@ extern char _code_end;
 
 typedef void (*voidfunc)();
 
+__attribute__((section("entry"))) void print_hex(uint32_t num) {
+    // 每个 16 进制字符代表 4 位，一共 8 个 hex 字符
+    for (int i = 7; i >= 0; i--) {
+        uint8_t nibble = (num >> (i * 4)) & 0xF;  // 取出每 4 位
+        char hex_char;
+
+        if (nibble < 10)
+            hex_char = '0' + nibble;
+        else
+            hex_char = 'A' + (nibble - 10);
+
+        putch(hex_char);
+    }
+    putch('\n');
+}
 
 __attribute__((section("entry"))) void _first_bootloader()
 {
+    device_ctrl uart_lcr = (device_ctrl)(DEV_SERIAL + 3);
+    *uart_lcr = 0x83;
+
+    device_ctrl uart_divisor = (device_ctrl)DEV_SERIAL;
+    uart_divisor[1] = 0x00;
+    uart_divisor[0] = 0x01;
+
+    *uart_lcr = 0x03;
+
     uint8_t *d = (uint8_t*)&_ssbl_start;
     const uint8_t *s = (uint8_t*)DEV_SRAM;
     uint32_t n = (uintptr_t)&_ssbl_end - (uintptr_t)&_ssbl_start;
 
     while (n--) {
+        print_hex(n);
         *d++ = *s++;
     }
 
@@ -38,7 +63,7 @@ __attribute__((section("ssbl"))) void _second_bootloader()
     while (n--) {
         *d++ = *s++;
     }
-    
+
     voidfunc start = (voidfunc)(DEV_PSRAM);
     start();
 }
