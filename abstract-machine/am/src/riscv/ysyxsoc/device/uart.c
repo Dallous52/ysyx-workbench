312452ee4ac196673ae88fb1e9be47e589f43081
tracer-ysyx <tracer@ysyx.org>
2025-07-17 10:21:00 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:21:00 up  1:23,  1 user,  load average: 0.33, 0.33, 0.25

--- abstract-machine/am/src/riscv/ysyxsoc/device/uart.c
+++ abstract-machine/am/src/riscv/ysyxsoc/device/uart.c
@@ -7,7 +7,7 @@ void __am_uart_init()
 
   device_ctrl uart_divisor = (device_ctrl)DEV_SERIAL;
   uart_divisor[1] = 0x00;
-  uart_divisor[0] = 0x46;
+  uart_divisor[0] = 0x01;
 
   *uart_lcr = 0x03;
 }
