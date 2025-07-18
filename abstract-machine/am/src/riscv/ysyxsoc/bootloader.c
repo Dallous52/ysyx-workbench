2b900563751ad152109d6bed5eb987ad36bdb325
tracer-ysyx <tracer@ysyx.org>
2025-07-18 11:09:39 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:09:39 up  2:48,  1 user,  load average: 0.37, 0.73, 0.70

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -23,11 +23,11 @@ extern char _data_ed;
 
 typedef void (*voidfunc)();
 
-// __attribute__((section("ssbl"))) void putch_(char ch) {
-//   volatile uint8_t* uart_lsr = (volatile uint8_t*)(DEV_SERIAL + 5);
-//   while (!(*uart_lsr & 0x20));
-//   outb(DEV_SERIAL, ch);
-// }
+__attribute__((section("ssbl"))) void putch_(char ch) {
+  volatile uint8_t* uart_lsr = (volatile uint8_t*)(DEV_SERIAL + 5);
+  while (!(*uart_lsr & 0x20));
+  outb(DEV_SERIAL, ch);
+}
 
 
 // __attribute__((section("ssbl"))) void print_hex_(uint32_t num) {
@@ -63,14 +63,14 @@ __attribute__((section("entry"))) void _first_bootloader()
 
 __attribute__((section("ssbl.boot"))) void _second_bootloader()
 {
-    // device_ctrl uart_lcr = (device_ctrl)(DEV_SERIAL + 3);
-    // *uart_lcr = 0x83;
+    device_ctrl uart_lcr = (device_ctrl)(DEV_SERIAL + 3);
+    *uart_lcr = 0x83;
 
-    // device_ctrl uart_divisor = (device_ctrl)DEV_SERIAL;
-    // uart_divisor[1] = 0x00;
-    // uart_divisor[0] = 0x01;
+    device_ctrl uart_divisor = (device_ctrl)DEV_SERIAL;
+    uart_divisor[1] = 0x00;
+    uart_divisor[0] = 0x01;
 
-    // *uart_lcr = 0x03;
+    *uart_lcr = 0x03;
     
     // 代码加载
     uint8_t *d = (uint8_t*)&_code_op;
@@ -97,5 +97,12 @@ __attribute__((section("ssbl.boot"))) void _second_bootloader()
     }
 
     voidfunc start = (voidfunc)(&_code_op);
+    char notice[] = "bootloader success, call to program.";
+    char* put = notice;
+    while (*put)
+    {
+        putch_(*put);
+        put++;
+    } 
     start();
 }
