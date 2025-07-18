068879e51fc5d4a380230a3130eea7895a4c36ca
tracer-ysyx <tracer@ysyx.org>
2025-07-18 10:12:26 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:12:26 up  1:51,  1 user,  load average: 0.07, 0.08, 0.14

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -15,28 +15,28 @@ extern char _code_ed;
 
 typedef void (*voidfunc)();
 
-// __attribute__((section("ssbl"))) void putch_(char ch) {
-//   volatile uint8_t* uart_lsr = (volatile uint8_t*)(DEV_SERIAL + 5);
-//   while (!(*uart_lsr & 0x20));
-//   outb(DEV_SERIAL, ch);
-// }
+__attribute__((section("ssbl"))) void putch_(char ch) {
+  volatile uint8_t* uart_lsr = (volatile uint8_t*)(DEV_SERIAL + 5);
+  while (!(*uart_lsr & 0x20));
+  outb(DEV_SERIAL, ch);
+}
 
 
-// __attribute__((section("ssbl"))) void print_hex_(uint32_t num) {
-//     // 每个 16 进制字符代表 4 位，一共 8 个 hex 字符
-//     for (int i = 7; i >= 0; i--) {
-//         uint8_t nibble = (num >> (i * 4)) & 0xF;  // 取出每 4 位
-//         char hex_char;
+__attribute__((section("ssbl"))) void print_hex_(uint32_t num) {
+    // 每个 16 进制字符代表 4 位，一共 8 个 hex 字符
+    for (int i = 7; i >= 0; i--) {
+        uint8_t nibble = (num >> (i * 4)) & 0xF;  // 取出每 4 位
+        char hex_char;
 
-//         if (nibble < 10)
-//             hex_char = '0' + nibble;
-//         else
-//             hex_char = 'A' + (nibble - 10);
+        if (nibble < 10)
+            hex_char = '0' + nibble;
+        else
+            hex_char = 'A' + (nibble - 10);
 
-//         putch_(hex_char);
-//     }
-//     putch_('\n');
-// }
+        putch_(hex_char);
+    }
+    putch_('\n');
+}
 
 __attribute__((section("entry"))) void _first_bootloader()
 {
@@ -55,22 +55,22 @@ __attribute__((section("entry"))) void _first_bootloader()
 
 __attribute__((section("ssbl.boot"))) void _second_bootloader()
 {
-    // device_ctrl uart_lcr = (device_ctrl)(DEV_SERIAL + 3);
-    // *uart_lcr = 0x83;
+    device_ctrl uart_lcr = (device_ctrl)(DEV_SERIAL + 3);
+    *uart_lcr = 0x83;
 
-    // device_ctrl uart_divisor = (device_ctrl)DEV_SERIAL;
-    // uart_divisor[1] = 0x00;
-    // uart_divisor[0] = 0x01;
+    device_ctrl uart_divisor = (device_ctrl)DEV_SERIAL;
+    uart_divisor[1] = 0x00;
+    uart_divisor[0] = 0x01;
 
-    // *uart_lcr = 0x03;
+    *uart_lcr = 0x03;
 
     uint8_t *d = (uint8_t*)DEV_PSRAM;
     const uint8_t *s = (uint8_t*)&_code_start;
     uint32_t n = (uintptr_t)&_code_ed - (uintptr_t)&_code_op;
 
-    // print_hex_((uintptr_t)&_code_start);
-    // print_hex_((uintptr_t)&_code_op);
-    // print_hex_((uintptr_t)&_code_ed);
+    print_hex_((uintptr_t)&_code_start);
+    print_hex_((uintptr_t)&_code_op);
+    print_hex_((uintptr_t)&_code_ed);
     
     while (n--) {
         *d++ = *s++;
