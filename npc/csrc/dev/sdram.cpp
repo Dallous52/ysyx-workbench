3284a070347449b805cc82247434088a1b968cd2
tracer-ysyx <tracer@ysyx.org>
2025-07-19 15:43:17 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:43:17 up  7:48,  1 user,  load average: 0.30, 0.29, 0.27

--- npc/csrc/dev/sdram.cpp
+++ npc/csrc/dev/sdram.cpp
@@ -18,15 +18,16 @@ word_t sdram_read_expr(word_t addr)
 
 extern "C" void sdram_row_load(int8_t bank, int16_t row, int16_t *data)
 {
-    for (int i = 0; i < 512; i++) data[i] =  sdram[bank][row][i];
+    for (int i = 0; i < 512; i++) data[i] = sdram[bank][row][i];
     printf(ANSI_FMT("[read sdram] bank:%d  row:0x%04x;\n", ANSI_FG_CYAN),
 			bank, row);
 }
 
 
-extern "C" void sdram_row_store(int8_t bank, int16_t row, int16_t **data)
+extern "C" void sdram_row_store(int8_t bank, int16_t row, int16_t *data)
 {
-    for (int i = 0; i < 512; i++) sdram[bank][row][i] = (*data)[i];
+    printf("%04x %04x\n", data[0], data[1]);
+    for (int i = 0; i < 512; i++) sdram[bank][row][i] = data[i];
     printf(ANSI_FMT("[write sdram] bank:%d  row:0x%04x;\n", ANSI_FG_CYAN),
 			bank, row);
 }
