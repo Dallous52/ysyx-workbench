a64fb68fd23e67e5d21736f3d1e7db7c6235d6bd
tracer-ysyx <tracer@ysyx.org>
2025-07-20 15:15:45 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:15:45 up  7:10,  1 user,  load average: 0.09, 0.38, 0.57

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -24,28 +24,6 @@ extern char _data_ed;
 
 typedef void (*voidfunc)();
 
-__attribute__((section("ssbl"))) void putch_(char ch) {
-  volatile uint8_t* uart_lsr = (volatile uint8_t*)(DEV_SERIAL + 5);
-  while (!(*uart_lsr & 0x20));
-  outb(DEV_SERIAL, ch);
-}
-
-
-__attribute__((section("ssbl"))) void print_hex_(uint32_t num) {
-    // 每个 16 进制字符代表 4 位，一共 8 个 hex 字符
-    for (int i = 7; i >= 0; i--) {
-        uint8_t nibble = (num >> (i * 4)) & 0xF;  // 取出每 4 位
-        char hex_char;
-
-        if (nibble < 10)
-            hex_char = '0' + nibble;
-        else
-            hex_char = 'A' + (nibble - 10);
-
-        putch_(hex_char);
-    }
-    putch_('\n');
-}
 
 __attribute__((section("entry"))) void _first_bootloader()
 {
@@ -59,6 +37,49 @@ __attribute__((section("entry"))) void _first_bootloader()
 
     voidfunc ssbl = (voidfunc)(&_ssbl_op);
     ssbl();
+}
+
+
+__attribute__((section("ssbl"))) void putch_(char ch) {
+  volatile uint8_t* uart_lsr = (volatile uint8_t*)(DEV_SERIAL + 5);
+  while (!(*uart_lsr & 0x20));
+  outb(DEV_SERIAL, ch);
+}
+
+
+__attribute__((section("ssbl"))) void print_hex_(uint32_t num) {
+    for (int i = 7; i >= 0; i--) {
+        uint8_t nibble = (num >> (i * 4)) & 0xF;
+        char hex_char;
+
+        if (nibble < 10)
+            hex_char = '0' + nibble;
+        else
+            hex_char = 'A' + (nibble - 10);
+
+        putch_(hex_char);
+    }
+    putch_('\n');
+}
+
+
+__attribute__((section("ssbl"))) void loader(uint8_t* dst, const uint8_t* src, uintptr_t len) 
+{
+    uint32_t *d = (uint32_t *)dst;
+    const uint32_t *s = (uint32_t *)src;
+    uintptr_t cnt32 = len / 4;
+
+    while (cnt32--) {
+        *d++ = *s++;
+    }
+
+    dst = (uint8_t *)d;
+    src = (const uint8_t *)s;
+    uintptr_t rem = len % 4;
+
+    while (rem--) {
+        *dst++ = *src++;
+    }
 }
 
 
@@ -80,9 +101,7 @@ __attribute__((section("ssbl.boot"))) void _second_bootloader()
     uint8_t *d = (uint8_t*)&_code_op;
     const uint8_t *s = (uint8_t*)&_code_start;
     uint32_t n = (uintptr_t)&_code_ed - (uintptr_t)&_code_op;
-    while (n--) {
-        *d++ = *s++;
-    }
+    loader(d, s, n);
 
     print_hex_((uintptr_t)&_rodata_op);
     print_hex_((uintptr_t)&_rodata_ed);
@@ -91,19 +110,15 @@ __attribute__((section("ssbl.boot"))) void _second_bootloader()
     d = (uint8_t*)&_rodata_op;
     s = (uint8_t*)&_rodata_start;
     n = (uintptr_t)&_rodata_ed - (uintptr_t)&_rodata_op;
-    while (n--) {
-        *d++ = *s++;
-    }
-    
+    loader(d, s, n);
+
     print_hex_((uintptr_t)&_data_op);
     print_hex_((uintptr_t)&_data_ed);
     // 全局变量加载
     d = (uint8_t*)&_data_op;
     s = (uint8_t*)&_data_start;
     n = (uintptr_t)&_data_ed - (uintptr_t)&_data_op;
-    while (n--) {
-        *d++ = *s++;
-    }
+    loader(d, s, n);
 
     print_hex_(0);
 
