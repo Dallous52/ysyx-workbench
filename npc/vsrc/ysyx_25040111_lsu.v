79533e8c9f7407ef2e6d521944e23b4a2027896b
tracer-ysyx <tracer@ysyx.org>
2025-07-20 14:58:09 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:58:09 up  6:52,  1 user,  load average: 0.96, 0.61, 0.38

--- npc/vsrc/ysyx_25040111_lsu.v
+++ npc/vsrc/ysyx_25040111_lsu.v
@@ -155,8 +155,8 @@ module ysyx_25040111_lsu (
             valid_t <= 1;
             rmem <= is_clint ? rmem_clint : io_master_rdata;
             // rready <= 0;
-            if (addr >= 32'ha000_0000 & addr <= 32'ha001_0000)
-                $display("raddr:%h  rdata:%h", addr, io_master_rdata);
+            // if (addr >= 32'ha000_0000& addr <= 32'ha001_0000)
+            //     $display("raddr:%h  rdata:%h", addr, io_master_rdata);
         end
     end
 
@@ -174,8 +174,8 @@ module ysyx_25040111_lsu (
 
         // 写入参数
         if (wvalid & wready) begin
-            if (addr >= 32'ha000_0000 & addr <= 32'ha001_0000) 
-                $display("waddr:%h  wdata:%h", addr, io_master_wdata);
+            // if (addr >= 32'ha000_0000 & addr <= 32'ha001_0000) 
+            //     $display("waddr:%h  wdata:%h", addr, io_master_wdata);
             wvalid <= 0;
             wlast <= 1;
         end
