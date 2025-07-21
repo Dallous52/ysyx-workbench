3e6cba3e76123c29f13f971cb1bb42edffb637b9
tracer-ysyx <tracer@ysyx.org>
2025-07-21 13:45:12 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:45:12 up  5:11,  1 user,  load average: 0.72, 0.46, 0.26

--- npc/csrc/dev/sdram.cpp
+++ npc/csrc/dev/sdram.cpp
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <svdpi.h>
 
-static uint16_t sdram[4][8192][512] __attribute((aligned(4096))) = {};
+static uint16_t sdram[4][8192][512][2] __attribute((aligned(4096))) = {};
 
 
 word_t sdram_read_expr(word_t addr)
@@ -16,28 +16,28 @@ word_t sdram_read_expr(word_t addr)
     int bank = (addr / 512) % 4;
     int row = addr / 2048;
     int idx = addr % 512;
-    return sdram[bank][row][idx];
+    return ((uint32_t)sdram[bank][row][idx][1] << 16) | (uint32_t)sdram[bank][row][idx][0];
 }
 
 
-extern "C" void sdram_row_load(int8_t bank, int16_t row, int16_t *data)
+extern "C" void sdram_row_load(int8_t bank, int16_t row, int16_t *data, int8_t raw)
 {
     int n = svSize(data, 1);
     uint16_t *p0 = (uint16_t*)svGetArrayPtr(data);
 
-    for (int i = 0; i < n; i++) p0[i] = sdram[bank][row][i];
+    for (int i = 0; i < n; i++) p0[i] = sdram[bank][row][i][raw];
  
     // printf(ANSI_FMT("[read sdram] bank:%d  row:0x%04x  num:%d;\n", ANSI_FG_CYAN),
 	// 		bank, row, n);
 }
 
 
-extern "C" void sdram_row_store(int8_t bank, int16_t row, const svOpenArrayHandle data)
+extern "C" void sdram_row_store(int8_t bank, int16_t row, const svOpenArrayHandle data, int8_t raw)
 {
     int n = svSize(data, 1);
     uint16_t *p0 = (uint16_t*)svGetArrayPtr(data);
 
-    for (int i = 0; i < n; i++) sdram[bank][row][i] = p0[i];
+    for (int i = 0; i < n; i++) sdram[bank][row][i][raw] = p0[i];
 
     // printf(ANSI_FMT("[write sdram] bank:%d  row:0x%04x  num:%d;\n", ANSI_FG_CYAN),
 	// 		bank, row, n);
