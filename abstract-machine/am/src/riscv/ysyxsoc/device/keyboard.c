d8f3e5a73817719c1c5278e3a95e9e4c062810c6
tracer-ysyx <tracer@ysyx.org>
2025-07-24 13:26:46 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:26:46 up  5:44,  1 user,  load average: 0.51, 0.42, 0.32

--- abstract-machine/am/src/riscv/ysyxsoc/device/keyboard.c
+++ abstract-machine/am/src/riscv/ysyxsoc/device/keyboard.c
@@ -125,7 +125,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
     else if (ps2code == 0xE0) 
     {
       ps2code = inb(DEV_KEYBOARD);
-      printf("E0 %02x\n", ps2code);
+      if (ps2code == 0xF0) ps2code = inb(DEV_KEYBOARD);
       code = ext_code[ps2code];
       break;
     }
