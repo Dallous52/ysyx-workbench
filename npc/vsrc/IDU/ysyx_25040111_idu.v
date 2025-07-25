d245fb7beabcc83b518e0c8a63641bca05ea5be9
tracer-ysyx <tracer@ysyx.org>
2025-07-25 15:33:49 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:33:49 up  6:54,  1 user,  load average: 0.14, 0.27, 0.23

--- npc/vsrc/IDU/ysyx_25040111_idu.v
+++ npc/vsrc/IDU/ysyx_25040111_idu.v
@@ -233,24 +233,6 @@ module ysyx_25040111_idu(
         7'b1100011, opt_branch
     });
 
-`ifdef PMC_EN
-    always @(*) begin
-        case (inst[6:0])
-        7'b0010011: monitor_counter(`IOPT);            
-        7'b0010111: monitor_counter(`IOPT);            
-        7'b0110111: monitor_counter(`IOPT);            
-        7'b1100111: monitor_counter(`IJUMP);            
-        7'b1101111: monitor_counter(`IJUMP);            
-        7'b1110011: monitor_counter(`ICSR);            
-        7'b0100011: monitor_counter(`IMEM);            
-        7'b0000011: monitor_counter(`IMEM);            
-        7'b0110011: monitor_counter(`IOPT);            
-        7'b1100011: monitor_counter(`IJUMP);            
-        default: ;
-        endcase
-    end
-`endif
-
 endmodule
 
 `endif
--- npc/vsrc/ysyx_25040111_ifu.v
+++ npc/vsrc/ysyx_25040111_ifu.v
@@ -39,14 +39,34 @@ module ysyx_25040111_ifu (
             inst <= inst_t;
             if_flag <= 0;
             valid <= 1;
-            `ifdef PMC_EN
+        `ifdef PMC_EN
             monitor_counter(`IFU);            
-            `endif
+        `endif
         end
         else inst <= inst;
 
         if (valid)
             valid <= 0;
     end
+
+`ifdef PMC_EN
+    always @(*) begin
+        if (if_ok) begin
+            case (inst[6:0])
+                7'b0010011: monitor_counter(`IOPT);            
+                7'b0010111: monitor_counter(`IOPT);            
+                7'b0110111: monitor_counter(`IOPT);            
+                7'b1100111: monitor_counter(`IJUMP);            
+                7'b1101111: monitor_counter(`IJUMP);            
+                7'b1110011: monitor_counter(`ICSR);            
+                7'b0100011: monitor_counter(`IMEM);            
+                7'b0000011: monitor_counter(`IMEM);            
+                7'b0110011: monitor_counter(`IOPT);            
+                7'b1100011: monitor_counter(`IJUMP);            
+                default: ;
+            endcase
+        end
+    end
+`endif
 
 endmodule
