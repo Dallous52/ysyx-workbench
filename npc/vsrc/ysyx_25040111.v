8071fa04fc3bb2124bff31fdd34b130b51e54b13
tracer-ysyx <tracer@ysyx.org>
2025-07-27 14:58:41 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:58:41 up  5:43,  1 user,  load average: 0.06, 0.15, 0.14

--- npc/vsrc/ysyx_25040111.v
+++ npc/vsrc/ysyx_25040111.v
@@ -2,81 +2,87 @@
 `include "IDU/ysyx_25040111_idu.v"
 
 module ysyx_25040111(
-        input clock,
-        input reset,
-        input io_interrupt,
+    input clock,
+    input reset,
 
-        output [31:0] pc,
-        output [31:0] inst,
+    output [31:0] pc,
+    output [31:0] inst
+`ifdef RUNSOC
+    ,input io_interrupt,
 
-        input io_master_awready,
-        output io_master_awvalid,
-        output [31:0] io_master_awaddr,
-        output [3:0] io_master_awid,
-        output [7:0] io_master_awlen,
-        output [2:0] io_master_awsize,
-        output [1:0] io_master_awburst,
+    input io_master_awready,
+    output io_master_awvalid,
+    output [31:0] io_master_awaddr,
+    output [3:0] io_master_awid,
+    output [7:0] io_master_awlen,
+    output [2:0] io_master_awsize,
+    output [1:0] io_master_awburst,
 
-        input io_master_wready,
-        output io_master_wvalid,
-        output [31:0] io_master_wdata,
-        output [3:0] io_master_wstrb,
-        output io_master_wlast,
+    input io_master_wready,
+    output io_master_wvalid,
+    output [31:0] io_master_wdata,
+    output [3:0] io_master_wstrb,
+    output io_master_wlast,
 
-        output io_master_bready,
-        input io_master_bvalid,
-        input [1:0] io_master_bresp,
-        input [3:0] io_master_bid,
+    output io_master_bready,
+    input io_master_bvalid,
+    input [1:0] io_master_bresp,
+    input [3:0] io_master_bid,
 
-        input io_master_arready,
-        output io_master_arvalid,
-        output [31:0] io_master_araddr,
-        output [3:0] io_master_arid,
-        output [7:0] io_master_arlen,
-        output [2:0] io_master_arsize,
-        output [1:0] io_master_arburst,
+    input io_master_arready,
+    output io_master_arvalid,
+    output [31:0] io_master_araddr,
+    output [3:0] io_master_arid,
+    output [7:0] io_master_arlen,
+    output [2:0] io_master_arsize,
+    output [1:0] io_master_arburst,
 
-        output io_master_rready,
-        input io_master_rvalid,
-        input [1:0] io_master_rresp,
-        input [31:0] io_master_rdata,
-        input io_master_rlast,
-        input [3:0] io_master_rid,
+    output io_master_rready,
+    input io_master_rvalid,
+    input [1:0] io_master_rresp,
+    input [31:0] io_master_rdata,
+    input io_master_rlast,
+    input [3:0] io_master_rid,
 
-        output io_slave_awready,
-        input io_slave_awvalid,
-        input [31:0] io_slave_awaddr,
-        input [3:0] io_slave_awid,
-        input [7:0] io_slave_awlen,
-        input [2:0] io_slave_awsize,
-        input [1:0] io_slave_awburst,
+    output io_slave_awready,
+    input io_slave_awvalid,
+    input [31:0] io_slave_awaddr,
+    input [3:0] io_slave_awid,
+    input [7:0] io_slave_awlen,
+    input [2:0] io_slave_awsize,
+    input [1:0] io_slave_awburst,
 
-        output io_slave_wready,
-        input io_slave_wvalid,
-        input [31:0] io_slave_wdata,
-        input [3:0] io_slave_wstrb,
-        input io_slave_wlast,
+    output io_slave_wready,
+    input io_slave_wvalid,
+    input [31:0] io_slave_wdata,
+    input [3:0] io_slave_wstrb,
+    input io_slave_wlast,
 
-        input io_slave_bready,
-        output io_slave_bvalid,
-        output [1:0] io_slave_bresp,
-        output [3:0] io_slave_bid,
+    input io_slave_bready,
+    output io_slave_bvalid,
+    output [1:0] io_slave_bresp,
+    output [3:0] io_slave_bid,
 
-        output io_slave_arready,
-        input io_slave_arvalid,
-        input [31:0] io_slave_araddr,
-        input [3:0] io_slave_arid,
-        input [7:0] io_slave_arlen,
-        input [2:0] io_slave_arsize,
-        input [1:0] io_slave_arburst,
+    output io_slave_arready,
+    input io_slave_arvalid,
+    input [31:0] io_slave_araddr,
+    input [3:0] io_slave_arid,
+    input [7:0] io_slave_arlen,
+    input [2:0] io_slave_arsize,
+    input [1:0] io_slave_arburst,
 
-        input io_slave_rready,
-        output io_slave_rvalid,
-        output [1:0] io_slave_rresp,
-        output [31:0] io_slave_rdata,
-        output io_slave_rlast,
-        output [3:0] io_slave_rid
-    );
+    input io_slave_rready,
+    output io_slave_rvalid,
+    output [1:0] io_slave_rresp,
+    output [31:0] io_slave_rdata,
+    output io_slave_rlast,
+    output [3:0] io_slave_rid
+`endif // RUNSOC
+);
+
+// ------------------------------------------------
+//                  IO SIGNAL DEFINE
+// ------------------------------------------------
 
     wire [4:0] rs1;
     wire [4:0] rs2;
@@ -168,8 +174,9 @@ module ysyx_25040111(
         .addr  	(lsu_addr      ),
         .wdata 	(rs2_d      ),
         .rdata 	(lsu_rdata      ),
-        .valid  (lsu_ok),
-        .io_master_awready 	(io_master_awready  ),
+        .valid  (lsu_ok)
+`ifdef RUNSOC
+        ,.io_master_awready (io_master_awready  ),
         .io_master_awvalid 	(io_master_awvalid  ),
         .io_master_awaddr  	(io_master_awaddr   ),
         .io_master_awid    	(io_master_awid     ),
@@ -198,6 +205,7 @@ module ysyx_25040111(
         .io_master_rdata   	(io_master_rdata    ),
         .io_master_rlast   	(io_master_rlast    ),
         .io_master_rid     	(io_master_rid      )
+`endif // RUNSOC
     );
 
     assign rdata = if_flag ? 32'b0 : lsu_rdata;
