b7cebc7aa9595d8c94135efd7b2148be565ab1af
tracer-ysyx <tracer@ysyx.org>
2025-07-28 10:40:51 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:40:51 up  2:54,  1 user,  load average: 0.41, 0.22, 0.18

--- npc/csrc/dev/pmc.cpp
+++ npc/csrc/dev/pmc.cpp
@@ -44,6 +44,7 @@ void cycle_counter(word_t inst, int64_t ncyc)
 void pmc_print()
 {
     int64_t all_inst = 0, all_cycle = 0;
+    double cpi = 0.;
 
     printf(ANSI_FG_BLUE "%-6s\t%10s\t%10s\t%5s\n" ANSI_NONE,
         "type", "inst", "cycle", "cpi");
@@ -52,10 +53,12 @@ void pmc_print()
     {
         all_inst += counter[i];
         all_cycle += cycnum[i];
-        printf(ANSI_FMT("%-6s\t%10d\t%10ld\t%5ld", ANSI_FG_GREEN) "\n", 
-            type_name[i], counter[i], cycnum[i], cycnum[i] / counter[i]);
+        cpi = counter[i] ? (double)cycnum[i] / counter[i] : 0.;
+        printf(ANSI_FMT("%-6s\t%10d\t%10ld\t%5lf", ANSI_FG_GREEN) "\n", 
+            type_name[i], counter[i], cycnum[i], cpi);
     }
 
-    printf(ANSI_FMT("%-6s\t%10ld\t%10ld\t%5ld", ANSI_FG_GREEN) "\n", 
-        "ALL", all_inst, all_cycle, all_cycle / all_inst);
+    cpi = all_inst ? (double)all_cycle / all_inst : 0.;
+    printf(ANSI_FMT("%-6s\t%10ld\t%10ld\t%5lf", ANSI_FG_GREEN) "\n", 
+        "ALL", all_inst, all_cycle, cpi);
 }
