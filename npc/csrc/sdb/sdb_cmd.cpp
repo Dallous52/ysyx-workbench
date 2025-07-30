f891b2b326c8e60d29bc1a2ab649d54c39a1b0fb
tracer-ysyx <tracer@ysyx.org>
2025-07-30 14:57:47 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:57:47 up  2:02,  1 user,  load average: 0.30, 0.26, 0.20

--- npc/csrc/sdb/sdb_cmd.cpp
+++ npc/csrc/sdb/sdb_cmd.cpp
@@ -205,7 +205,7 @@ int cmd_v(char* args)
 int cmd_csim(char* args)
 {
   int cls = 2, bls = 4;
-  if (scanf("%d %d", &cls, &bls) != 2)
+  if (sscanf(args, "%d %d", &cls, &bls) != 2)
   {
     printf("Please use command: \"csim [cache sqrt num] [block ...]\".\n");
     return 0;
