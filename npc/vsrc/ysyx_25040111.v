36bccc84741b413baf5314f27ff8953a6e2c55ce
tracer-ysyx <tracer@ysyx.org>
2025-08-01 10:51:39 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:51:39 up  3:02,  1 user,  load average: 0.15, 0.26, 0.21

--- npc/vsrc/ysyx_25040111.v
+++ npc/vsrc/ysyx_25040111.v
@@ -136,7 +136,9 @@ module ysyx_25040111(
         .valid  	(icache_valid   ),
         .ready  	(icache_ready   )
     );
-    
+    always @(posedge clock) begin
+        $display("flag: %b", if_flag);
+    end
 
     ysyx_25040111_idu u_idu (
         .inst 	(inst  ),
