d935d9b4f599a979185cc729738646eb38267805
tracer-ysyx <tracer@ysyx.org>
2025-08-04 08:42:42 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:42:42 up 41 min,  1 user,  load average: 0.42, 0.22, 0.13

--- npc/vsrc/IDU/ysyx_25040111_opimm.v
+++ npc/vsrc/IDU/ysyx_25040111_opimm.v
@@ -5,8 +5,8 @@ module ysyx_25040111_opimm (
     input [31:7] inst,
     output [4:0] rs1,
     output [4:0] rd,
-    output reg [31:0] imm,
-    output reg [`OPT_HIGH:0] opt
+    output [31:0] imm,
+    output [`OPT_HIGH:0] opt
 );
 
     wire [11:0] imm_m;
@@ -14,26 +14,20 @@ module ysyx_25040111_opimm (
 
     assign {imm_m, rs1, fun3, rd} = inst[31:7];
 
-    always @(*) begin
-        imm = {{20{imm_m[11]}}, imm_m};
-        opt = `OPT_LEN'b0;
+    ysyx_25040111_MuxKeyWithDefault #(2, 3, 32) imm_c (imm, fun3, {{20{imm_m[11]}}, imm_m}, {
+        3'b001, {27'b0, imm_m[4:0]},
+        3'b101, {27'b0, imm_m[4:0]}
+    });
 
-        case (fun3)
-            3'b000: opt = `OPTG(`WFX, `RF_IM, `ADD, `SNPC, `EMPTY, `EMPTY);     // addi
-            3'b011: opt = `OPTG(`WFX, `RF_IM, `COMPARE, `SNPC, `EMPTY, `EXX);   // sltiu
-            3'b010: opt = `OPTG(`WFX, `RF_IM, `COMPARE, `SNPC, `EMPTY, `ESX);   // slti
-            3'b101: opt = `OPTG(`WFX, `RF_IM, `RSHIFT, `SNPC, `EMPTY, {1'b0, inst[30], 1'b0}); // srai srli
-            3'b111: opt = `OPTG(`WFX, `RF_IM, `AND, `SNPC, `EMPTY, `EMPTY);     // andi
-            3'b100: opt = `OPTG(`WFX, `RF_IM, `XOR, `SNPC, `EMPTY, `EMPTY);     // xori
-            3'b001: begin
-                opt = `OPTG(`WFX, `RF_IM, `LSHIFT, `SNPC, `EMPTY, `EMPTY);  // slli
-                imm = {27'b0, imm_m[4:0]};
-            end
-            3'b110: begin
-                opt = `OPTG(`WFX, `RF_IM, `AND, `SNPC, `EMPTY, `EXX);       // ori
-                imm = {27'b0, imm_m[4:0]};
-            end
-        endcase
-    end
+    ysyx_25040111_MuxKeyWithDefault #(8, 3, `OPT_LEN) opt_c (opt, fun3, `OPT_LEN'b0, {
+        3'b000, `OPTG(`WFX, `RF_IM, `ADD, `SNPC, `EMPTY, `EMPTY),       // addi
+        3'b011, `OPTG(`WFX, `RF_IM, `COMPARE, `SNPC, `EMPTY, `EXX),     // sltiu
+        3'b010, `OPTG(`WFX, `RF_IM, `COMPARE, `SNPC, `EMPTY, `ESX),     // slti
+        3'b101, `OPTG(`WFX, `RF_IM, `RSHIFT, `SNPC, `EMPTY, {1'b0, inst[30], 1'b0}), // srai srli
+        3'b111, `OPTG(`WFX, `RF_IM, `AND, `SNPC, `EMPTY, `EMPTY),       // andi
+        3'b100, `OPTG(`WFX, `RF_IM, `XOR, `SNPC, `EMPTY, `EMPTY),       // xori
+        3'b001, `OPTG(`WFX, `RF_IM, `LSHIFT, `SNPC, `EMPTY, `EMPTY),    // slli
+        3'b110, `OPTG(`WFX, `RF_IM, `AND, `SNPC, `EMPTY, `EXX)          // ori
+    });
 
 endmodule
