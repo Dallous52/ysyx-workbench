95f1b720ca6384aa3b986508e7bbef162ace89eb
tracer-ysyx <tracer@ysyx.org>
2025-08-04 08:47:43 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:47:43 up 46 min,  1 user,  load average: 0.11, 0.17, 0.13

--- npc/vsrc/IDU/ysyx_25040111_opimm.v
+++ npc/vsrc/IDU/ysyx_25040111_opimm.v
@@ -5,8 +5,8 @@ module ysyx_25040111_opimm (
     input [31:7] inst,
     output [4:0] rs1,
     output [4:0] rd,
-    output [31:0] imm,
-    output [`OPT_HIGH:0] opt
+    output reg [31:0] imm,
+    output reg [`OPT_HIGH:0] opt
 );
 
     wire [11:0] imm_m;
