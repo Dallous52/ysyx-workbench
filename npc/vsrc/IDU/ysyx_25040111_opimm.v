72fae73aec7716fcbda46f11b73c6e1c732fb28a
tracer-ysyx <tracer@ysyx.org>
2025-08-04 08:48:53 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:48:53 up 47 min,  1 user,  load average: 0.56, 0.31, 0.18

--- npc/vsrc/IDU/ysyx_25040111_opimm.v
+++ npc/vsrc/IDU/ysyx_25040111_opimm.v
@@ -1,5 +1,4 @@
 `include "../HDR/ysyx_25040111_inc.vh"
-`include "../MOD/ysyx_25040111_MuxKeyWithDefault.v"
 
 module ysyx_25040111_opimm (
     input [31:7] inst,
@@ -35,21 +34,5 @@ module ysyx_25040111_opimm (
             end
         endcase
     end
-
-    // ysyx_25040111_MuxKeyWithDefault #(2, 3, 32) imm_c (imm, fun3, {{20{imm_m[11]}}, imm_m}, {
-    //     3'b001, {27'b0, imm_m[4:0]},
-    //     3'b101, {27'b0, imm_m[4:0]}
-    // });
-
-    // ysyx_25040111_MuxKeyWithDefault #(8, 3, `OPT_LEN) opt_c (opt, fun3, `OPT_LEN'b0, {
-    //     3'b000, `OPTG(`WFX, `RF_IM, `ADD, `SNPC, `EMPTY, `EMPTY),       // addi
-    //     3'b011, `OPTG(`WFX, `RF_IM, `COMPARE, `SNPC, `EMPTY, `EXX),     // sltiu
-    //     3'b010, `OPTG(`WFX, `RF_IM, `COMPARE, `SNPC, `EMPTY, `ESX),     // slti
-    //     3'b101, `OPTG(`WFX, `RF_IM, `RSHIFT, `SNPC, `EMPTY, {1'b0, inst[30], 1'b0}), // srai srli
-    //     3'b111, `OPTG(`WFX, `RF_IM, `AND, `SNPC, `EMPTY, `EMPTY),       // andi
-    //     3'b100, `OPTG(`WFX, `RF_IM, `XOR, `SNPC, `EMPTY, `EMPTY),       // xori
-    //     3'b001, `OPTG(`WFX, `RF_IM, `LSHIFT, `SNPC, `EMPTY, `EMPTY),    // slli
-    //     3'b110, `OPTG(`WFX, `RF_IM, `AND, `SNPC, `EMPTY, `EXX)          // ori
-    // });
 
 endmodule
