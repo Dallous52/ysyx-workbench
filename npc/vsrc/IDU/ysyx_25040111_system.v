0418817edbe2870acd21695d367aa6e98e7e7dbf
tracer-ysyx <tracer@ysyx.org>
2025-08-04 09:09:34 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:09:34 up  1:08,  1 user,  load average: 0.93, 0.44, 0.36

--- npc/vsrc/IDU/ysyx_25040111_system.v
+++ npc/vsrc/IDU/ysyx_25040111_system.v
@@ -1,5 +1,4 @@
 `include "../HDR/ysyx_25040111_inc.vh"
-`include "../MOD/ysyx_25040111_MuxKeyWithDefault.v"
 
 `define SYS_EBREAk 12'h001
 `define SYS_ECALL  12'h000
@@ -11,7 +10,7 @@ module ysyx_25040111_system(
     output [4:0] rd,
     output [11:0] csr1, csr2,
     output [31:0] imm,
-    output [`OPT_HIGH:0] opt
+    output reg [`OPT_HIGH:0] opt
 );
     wire [2:0] fun3;
     wire fix;
@@ -23,28 +22,61 @@ module ysyx_25040111_system(
     assign rs1 = fix ?  5'd10 : inst[19:15];
     assign imm = 32'b0;
 
-    ysyx_25040111_MuxKeyWithDefault #(3, 13, 12) csr_wc (csr1, {inst[31:20], fix}, inst[31:20], {
-        {`SYS_EBREAk, 1'b1},    12'b0,
-        {`SYS_ECALL,  1'b1},    `MEPC,
-        {`SYS_MRET,   1'b1},    12'b0
-    });
+    always @(*) begin
+        csr1 = inst[31:20];
+        csr2 = inst[31:20];
+        case ({inst[31:20], fix})
+            {`SYS_EBREAK, 1'b1}: begin
+                csr1 = 12'b0;
+                csr2 = 12'b0;
+            end
+            {`SYS_ECALL,  1'b1}: begin
+                csr1 = `MEPC;
+                csr2 = `MTVEC;
+            end
+            {`SYS_MRET,   1'b1}: begin
+                csr1 = 12'b0;
+                csr2 = `MEPC;
+            end
+        endcase
 
-    ysyx_25040111_MuxKeyWithDefault #(3, 13, 12) csr_rc (csr2, {inst[31:20], fix}, inst[31:20], {
-        {`SYS_EBREAk, 1'b1},    12'b0,
-        {`SYS_ECALL,  1'b1},    `MTVEC,
-        {`SYS_MRET,   1'b1},    `MEPC
-    });
+        opt_t = `OPT_LEN'b0;
+        case (inst[31:20])
+            `SYS_EBREAK: opt_t = `EBREAK_INST;
+            `SYS_ECALL:  opt_t = `OPTG(`EMPTY, `PC_IM, `ADD, `JECALL, `WFS, `XXN);
+            `SYS_MRET:   opt_t = `OPTG(`EMPTY, `EMP, `EMPTY, `EMP, `XFS, `XXN);
+        endcase
 
-    ysyx_25040111_MuxKeyWithDefault #(3, 12, `OPT_LEN) opt_tc (opt_t, inst[31:20], `OPT_LEN'b0, {
-        `SYS_EBREAk,    `EBREAK_INST,
-        `SYS_ECALL,     `OPTG(`EMPTY, `PC_IM, `ADD, `JECALL, `WFS, `XXN),
-        `SYS_MRET,      `OPTG(`EMPTY, `EMP, `EMPTY, `EMP, `XFS, `XXN)
-    });
+        opt = `OPT_LEN'b0;
+        case (fun3)
+            3'b000: opt = opt_t;
+            3'b001: opt = `OPTG(`WFX, `RF_IM, `ADD,   `SNPC, `WFX, `XXN);
+            3'b010: opt = `OPTG(`WFX, `RF_RS, `AND,   `SNPC, `WFX, `EXN);
+        endcase
+    end
 
-    ysyx_25040111_MuxKeyWithDefault #(3, 3, `OPT_LEN) opt_c (opt, fun3, `OPT_LEN'b0, {
-        3'b000, opt_t,
-        3'b001, `OPTG(`WFX, `RF_IM, `ADD, `SNPC, `WFX, `XXN),
-        3'b010, `OPTG(`WFX, `RF_RS, `AND, `SNPC, `WFX, `EXN)
-    });
+    // ysyx_25040111_MuxKeyWithDefault #(3, 13, 12) csr_wc (csr1, {inst[31:20], fix}, inst[31:20], {
+    //     {`SYS_EBREAk, 1'b1},    12'b0,
+    //     {`SYS_ECALL,  1'b1},    `MEPC,
+    //     {`SYS_MRET,   1'b1},    12'b0
+    // });
+
+    // ysyx_25040111_MuxKeyWithDefault #(3, 13, 12) csr_rc (csr2, {inst[31:20], fix}, inst[31:20], {
+    //     {`SYS_EBREAk, 1'b1},    12'b0,
+    //     {`SYS_ECALL,  1'b1},    `MTVEC,
+    //     {`SYS_MRET,   1'b1},    `MEPC
+    // });
+
+    // ysyx_25040111_MuxKeyWithDefault #(3, 12, `OPT_LEN) opt_tc (opt_t, inst[31:20], `OPT_LEN'b0, {
+    //     `SYS_EBREAk,    `EBREAK_INST,
+    //     `SYS_ECALL,     `OPTG(`EMPTY, `PC_IM, `ADD, `JECALL, `WFS, `XXN),
+    //     `SYS_MRET,      `OPTG(`EMPTY, `EMP, `EMPTY, `EMP, `XFS, `XXN)
+    // });
+
+    // ysyx_25040111_MuxKeyWithDefault #(3, 3, `OPT_LEN) opt_c (opt, fun3, `OPT_LEN'b0, {
+    //     3'b000, opt_t,
+    //     3'b001, `OPTG(`WFX, `RF_IM, `ADD, `SNPC, `WFX, `XXN),
+    //     3'b010, `OPTG(`WFX, `RF_RS, `AND, `SNPC, `WFX, `EXN)
+    // });
 
 endmodule
