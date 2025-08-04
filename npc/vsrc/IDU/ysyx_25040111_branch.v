1047827bc1b6cd69e0064fb3cf37ea8e43d04f92
tracer-ysyx <tracer@ysyx.org>
2025-08-04 08:51:21 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:51:21 up 49 min,  1 user,  load average: 0.10, 0.21, 0.16

--- npc/vsrc/IDU/ysyx_25040111_branch.v
+++ npc/vsrc/IDU/ysyx_25040111_branch.v
@@ -1,12 +1,11 @@
 `include "../HDR/ysyx_25040111_inc.vh"
-`include "../MOD/ysyx_25040111_MuxKeyWithDefault.v"
 
 module ysyx_25040111_branch(
     input [31:7] inst,
     output [4:0] rs1,
     output [4:0] rs2,
     output [31:0] imm,
-    output [`OPT_HIGH:0] opt
+    output reg [`OPT_HIGH:0] opt
 );
 
     wire [12:0] imm_m;
@@ -21,13 +20,16 @@ module ysyx_25040111_branch(
     assign {rs2, rs1, fun3} = inst[24:12];
     assign imm = {{19{imm_m[12]}}, imm_m};
 
-    ysyx_25040111_MuxKeyWithDefault #(6, 3, `OPT_LEN) opt_c (opt, fun3, `OPT_LEN'b0, {
-        3'b000, `OPTG(`XFS, `RF_RS, `EQUAL, `BRANCH, `EMPTY, `EXX),   // beq
-        3'b001, `OPTG(`XFS, `RF_RS, `EQUAL, `BRANCH, `EMPTY, `EXN),   // bne
-        3'b111, `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `EXN), // bgeu
-        3'b100, `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `ESX), // blt
-        3'b101, `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `ESN), // bge
-        3'b110, `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `EXX)  // bltu
-    });
+    always @(*) begin
+        case (fun3)
+            3'b000: opt = `OPTG(`XFS, `RF_RS, `EQUAL,   `BRANCH, `EMPTY, `EXX); // beq
+            3'b001: opt = `OPTG(`XFS, `RF_RS, `EQUAL,   `BRANCH, `EMPTY, `EXN); // bne
+            3'b111: opt = `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `EXN); // bgeu
+            3'b100: opt = `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `ESX); // blt
+            3'b101: opt = `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `ESN); // bge
+            3'b110: opt = `OPTG(`XFS, `RF_RS, `COMPARE, `BRANCH, `EMPTY, `EXX); // bltu
+            default: opt = `OPT_LEN'b0;
+        endcase
+    end
 
 endmodule
