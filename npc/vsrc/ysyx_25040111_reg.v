4253ac092f9935bc32fcadc287907d60c3661a4c
tracer-ysyx <tracer@ysyx.org>
2025-08-11 16:26:03 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:26:03 up  7:43,  1 user,  load average: 0.06, 0.07, 0.09

--- npc/vsrc/ysyx_25040111_reg.v
+++ npc/vsrc/ysyx_25040111_reg.v
@@ -11,12 +11,12 @@ module ysyx_25040111_reg (
 );
 
     // 我们只保存寄存器 1..15（共 15 个 32-bit 单元），索引映射为 [0..14] 对应 arch[1..15]
-    reg  [31:0] rf15 [0:14];
+    reg  [31:0] rf [0:14];
 
     // 写：若写地址为 1..15 才写入
     always @(posedge clock) begin
         if (wen && (waddr != 4'd0)) begin
-            rf15[waddr - 1] <= wdata; // waddr in 1..15 maps to rf15[0..14]
+            rf[waddr - 1] <= wdata; // waddr in 1..15 maps to rf[0..14]
         end
     end
 
@@ -24,9 +24,9 @@ module ysyx_25040111_reg (
     wire forward1 = (wen && (waddr != 4'd0) && (ars1 == waddr));
     wire forward2 = (wen && (waddr != 4'd0) && (ars2 == waddr));
 
-    // 读：若 ars == 0 直接返回常数 0，否则从 rf15[ars-1] 读出
-    wire [31:0] rf_rs1 = (ars1 == 4'd0) ? 32'd0 : rf15[ars1 - 1];
-    wire [31:0] rf_rs2 = (ars2 == 4'd0) ? 32'd0 : rf15[ars2 - 1];
+    // 读：若 ars == 0 直接返回常数 0，否则从 rf[ars-1] 读出
+    wire [31:0] rf_rs1 = (ars1 == 4'd0) ? 32'd0 : rf[ars1 - 1];
+    wire [31:0] rf_rs2 = (ars2 == 4'd0) ? 32'd0 : rf[ars2 - 1];
 
     assign rs1 = ren[0] ? (forward1 ? wdata : rf_rs1) : 32'd0;
     assign rs2 = ren[1] ? (forward2 ? wdata : rf_rs2) : 32'd0;
