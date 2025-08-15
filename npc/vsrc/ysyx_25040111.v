bc47d30339514f3a0cf4286ee111a51f234a37e4
tracer-ysyx <tracer@ysyx.org>
2025-08-15 20:36:04 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:36:04 up 12:25,  1 user,  load average: 0.02, 0.17, 0.25

--- npc/vsrc/ysyx_25040111.v
+++ npc/vsrc/ysyx_25040111.v
@@ -200,7 +200,7 @@ module ysyx_25040111(
 
     // ICACHE
     ysyx_25040111_cache #(
-        .CACHE_Ls 	(1  ),
+        .CACHE_Ls 	(0  ),
         .BLOCK_Ls 	(4  ))
     u_icache(
         .clock  	(clock       ),
