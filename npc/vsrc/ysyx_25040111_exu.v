3ec8a2e8b031aa1ed1e83196634d7052c54917fb
tracer-ysyx <tracer@ysyx.org>
2025-08-15 18:20:31 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:20:31 up 10:03,  1 user,  load average: 0.05, 0.19, 0.15

--- npc/vsrc/ysyx_25040111_exu.v
+++ npc/vsrc/ysyx_25040111_exu.v
@@ -107,8 +107,13 @@ module ysyx_25040111_exu(
     // alu paramter
     reg  [31:0]         alu_p1, alu_p2;
     wire [31:0]         rd;
-    wire [6:0]          alu_ctrl = exe_end ? {`ADD, 1'b0, `EMPTY} : 
-                                    {opt[7:5], (opt[12:10]==3'b100), opt[15:13]};
+    wire [6:0]          alu_ctrl;
+    always @* begin
+    case (exe_end)
+        1'b1: alu_ctrl = {`ADD, 1'b0, `EMPTY};
+        1'b0: alu_ctrl = {opt[7:5], (opt[12:10]==3'b100), opt[15:13]};
+    endcase
+    end
     
     // read after write lock paramter
     wire lock = |(rlock & ((16'h1 << ard_in[3:0]) |
