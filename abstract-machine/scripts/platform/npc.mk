ff413fc8b3bbd597dec8d096eea0c34c75c2ee93
tracer-ysyx <tracer@ysyx.org>
2025-08-29 09:53:27 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:53:27 up 2 min,  1 user,  load average: 0.06, 0.03, 0.01

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -24,7 +24,7 @@ insert-arg: image
 
 image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
-	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@echo + OBJCOPY "->" $(IMAGE_REL).bind
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
