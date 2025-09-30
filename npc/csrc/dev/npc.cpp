99852da6583bb68732f2c1f187c1f0f14924e121
tracer-ysyx <tracer@ysyx.org>
2025-09-30 15:49:58 +0800

    > sim RTL ysyx_22040000 胡昊 Linux DallousMain 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:49:58 up  6:54,  1 user,  load average: 0.08, 0.10, 0.04

--- npc/csrc/dev/npc.cpp
+++ npc/csrc/dev/npc.cpp
@@ -345,6 +345,9 @@ extern "C" int pmem_read(int raddr)
 {
 	paddr_t address = raddr & ~0x3u;
 	word_t rdata = 0;
+		
+  printf(ANSI_FMT("[read mem] address: 0x%08x; data: 0x%08x; pc: 0x%08x;\n", ANSI_FG_CYAN),
+			(word_t)raddr, rdata, CPU_PC);
 
   if (likely(in_pmem(address)))
     rdata = paddr_read(address, 4);
